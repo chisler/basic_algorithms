83464bcd6ec756ad2bd4aee03163846fc6c05917
Alexander Chisler <alexander.chisler@gmail.com>
2017-04-05 11:04:17 +0300

    binary search for index added

--- binary_search/binary_search_good.py
+++ binary_search/binary_search_good.py
@@ -1,22 +1,32 @@
+def is_even(i):
+    return i % 2 == 0
+
+
+# [1, 2, 3, 4, 5] -> 0, 1     | l = 4
+# [1, 2, 3, 4, 5, 6] -> 0, 2  | l = 5
 def left_indexes(start, end):
-    return start, (end - start) // 2
+    l = end - start
+    return start, start + (l - 1) // 2
 
 
+# [1, 2, 3, 4, 5] -> 3, 4     | l = 4
+# [1, 2, 3, 4, 5, 6] -> 3, 5  | l = 5
 def right_indexes(start, end):
-    return (end - start) // 2, end
+    l = end - start
+    return start + l // 2 + 1, end
 
 
 def middle(array, start, end):
-    length = end - start + 1
+    l = end - start
 
-    if length % 2 == 0:
-        return (array[length // 2 - 1] + array[length // 2]) / 2, False
-    return array[length // 2], True
+    if not is_even(l):
+        return (array[start + l // 2] + array[start + l // 2] + 1) / 2, False
+    return array[start + l // 2], True
 
 
 def binary_search(array, element, start=0, end=None):
     if end is None:
-        end = len(array)
+        end = len(array) - 1
 
     if end - start < 1:
         return start if array[start] == element else -1
@@ -25,16 +35,18 @@ def binary_search(array, element, start=0, end=None):
 
     if element < mid:
         return binary_search(array, element, *left_indexes(start, end))
-    elif element > mid:
+
+    if element > mid:
         return binary_search(array, element, *right_indexes(start, end))
 
     if element == mid:
-        return array[(end - start) // 2] if present else -1
+        return (end - start) // 2 if present else -1
 
 
 array = [2, 3, 4, 1, 5]
 array = sorted(array)
 
+print(list(enumerate(array)))
+
 for i in range(10):
     print(binary_search(array, i))
-
